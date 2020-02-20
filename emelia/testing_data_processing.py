778e6271a50baa60d68a14a0956d2d39ad81c5f6
robotsarecrazy64 <jkr232@nau.edu>
2020-02-19 17:17:56 -0700

    Renamed files to improve readability

--- emelia/testing_data_processing.py
+++ emelia/testing_data_processing.py
@@ -14,17 +14,15 @@ def list_strict_equal (list_one, list_two):
 class MyTest(unittest.TestCase):
     # Tests the creation of the alarm hex master set
     def test_make_alarm_hex_master_set(self):
-        test_list_one = ['0x10009', '0x10703', '0x10F69', '0x47E016B', '0x47E0228', '0x47E02FE', '0x47E0310', '0x47E0319', '0x47E0320', '0x47E0323', '0x47E0324', '0x47E0326', '0x47E0329', '0x47E0330', '0x47E0339', '0x47E0349', '0x47E035D', '0x47E035F', '0x47E0360', '0x47E0361', '0x47E042D', '0x47E048C', '0x47E0560', '0x47E05FD', '0x47E05FF', '0x47E060D', '0x47E0616', '0x47E0690', '0x47E06C6', '0x47E06CB', '0x47E06D9', '0x47E06DB', '0x47E06DE', '0x47E06E0', '0x47E06E1', '0x47E06E2', '0x47E07B9', '0x47E07C6', '0x47E07CF', '0x47E07DC', '0x47E07DD', '0x47E07E1', '0x47E07E6', '0x47E07E9', '0x47E07EA', '0x47E07F2', '0x47E07F5', '0x47E07FA', '0x47E0812', '0x47E0822', '0x47E0854', '0x47E0857', '0x47E0859', '0x47E1006', '0x47E1007', '0x47E1008', '0x47E100C', '0x47E100D', '0x47E100E', '0x47E1013', '0x47E1014', '0x47E102D', '0x47E102E', '0x47E102F', '0x47E1030', '0x47E1045', '0x47E1046', '0x47E1047', '0x47E1049', '0x47E104E', '0x47E1052', '0x47E1058', '0x47E105A', '0x47E15FB' ] # master test array
-
-        test_list_two = [] # empty array
-
+        master_list = ['0x10009', '0x10703', '0x10F69', '0x47E016B', '0x47E0228', '0x47E02FE', '0x47E0310', '0x47E0319', '0x47E0320', '0x47E0323', '0x47E0324', '0x47E0326', '0x47E0329', '0x47E0330', '0x47E0339', '0x47E0349', '0x47E035D', '0x47E035F', '0x47E0360', '0x47E0361', '0x47E042D', '0x47E048C', '0x47E0560', '0x47E05FD', '0x47E05FF', '0x47E060D', '0x47E0616', '0x47E0690', '0x47E06C6', '0x47E06CB', '0x47E06D9', '0x47E06DB', '0x47E06DE', '0x47E06E0', '0x47E06E1', '0x47E06E2', '0x47E07B9', '0x47E07C6', '0x47E07CF', '0x47E07DC', '0x47E07DD', '0x47E07E1', '0x47E07E6', '0x47E07E9', '0x47E07EA', '0x47E07F2', '0x47E07F5', '0x47E07FA', '0x47E0812', '0x47E0822', '0x47E0854', '0x47E0857', '0x47E0859', '0x47E1006', '0x47E1007', '0x47E1008', '0x47E100C', '0x47E100D', '0x47E100E', '0x47E1013', '0x47E1014', '0x47E102D', '0x47E102E', '0x47E102F', '0x47E1030', '0x47E1045', '0x47E1046', '0x47E1047', '0x47E1049', '0x47E104E', '0x47E1052', '0x47E1058', '0x47E105A', '0x47E15FB' ] # master test array
+        empty_list = [] # empty array
         result = data_processing.make_alarm_hex_master_set() # function call
         self.assertEqual(len(result), 74) # the resulting list should have a length of 74
         self.assertFalse(result == 1583) # the resulting length should not be equal to the input length (1583 tickets)
-        self.assertEqual(result, test_list_one) # the resulting list should contain 74 unique hex codes
-        self.assertFalse(result == test_list_two) # the resulting list should not be empty
-        test_list_one.sort() # sort the master test array
-        self.assertTrue(list_strict_equal(test_list_one, result)) # the resulting array should be sorted
+        self.assertEqual(result, master_list) # the resulting list should contain 74 unique hex codes
+        self.assertFalse(result == empty_list) # the resulting list should not be empty
+        master_list.sort() # sort the master test array
+        self.assertTrue(list_strict_equal(master_list, result)) # the resulting array should be sorted
 
 if __name__ == '__main__':
     unittest.main()
